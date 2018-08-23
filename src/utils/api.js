84a37849e10bf039b12190b99c7bcc014706ba60
zhangjinpeng <zhangjinpeng@easy2study.net>
2018-08-23 23:47:18 +0800

    上传头像

--- src/utils/api.js
+++ src/utils/api.js
@@ -143,10 +143,35 @@ const logout = async (params = {}) => {
   return logoutResponse
 }
 
+// 上传图片
+const updateFile = async (options = {}) => {
+  // 显示loading
+  wepy.showLoading({title: '上传中'})
+
+  // 获取token
+  let accessToken = await getToken()
+
+  // 拼接url
+  options.url = host + '/' + options.url
+  let header = options.header || {}
+  // 将 token 设置在 header 中
+  header.Authorization = 'Bearer' + accessToken
+  options.header = header
+
+  // 上传文件
+  let response = await wepy.uploadFile(options)
+
+  // 隐藏loading
+  wepy.hideLoading()
+
+  return response
+}
+
 export default {
   request,
   authRequest,
   refreshToken,
   login,
-  logout
+  logout,
+  updateFile
 }
